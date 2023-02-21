6d6655454f9d8d22cb8209878b24f7b3690404c0
Edward Wu <bluehome.wu@gmail.com>
2023-02-22 00:57:21 +0800

    Add extras for qssi-user 11 PROS2203050CN00MR2 V12.0.1.0.RBICNBS release-keys
    Signed-off-by: Edward Wu <bluehome.wu@gmail.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# qssi-user 11 PROS2203050CN00MR2 V12.0.1.0.RBICNBS release-keys
+- manufacturer: xiaomiblackshark
+- platform: kona
+- codename: penrose
+- flavor: qssi-user
+- release: 11
+- id: PROS2203050CN00MR2
+- incremental: V12.0.1.0.RBICNBS
+- tags: release-keys
+- fingerprint: blackshark/PRS-A0/penrose:11/PROS2203050CN00MR2/V12.0.1.0.RBICNBS:user/release-keys
+blackshark/PRS-A0/penrose:11/PROS2203050CN00MR2/V12.0.1.0.RBICNBS:user/release-keys
+- is_ab: true
+- brand: blackshark
+- branch: qssi-user-11-PROS2203050CN00MR2-V12.0.1.0.RBICNBS-release-keys
+- repo: blackshark_penrose_dump
---
+++ twrp-device-tree/blackshark/penrose/omni_penrose.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from penrose device
+$(call inherit-product, device/blackshark/penrose/device.mk)
+
+PRODUCT_DEVICE := penrose
+PRODUCT_NAME := omni_penrose
+PRODUCT_BRAND := blackshark
+PRODUCT_MODEL := SHARK PRS-A0
+PRODUCT_MANUFACTURER := blackshark
+
+PRODUCT_GMS_CLIENTID_BASE := android-blackshark
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="PRS-A0-user 11 PROS2203050CN00MR2 V12.0.1.0.RBICNBS release-keys"
+
+BUILD_FINGERPRINT := blackshark/PRS-A0/penrose:11/PROS2203050CN00MR2/V12.0.1.0.RBICNBS:user/release-keys
